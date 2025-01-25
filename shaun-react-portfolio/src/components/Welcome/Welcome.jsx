8e02416ff416d5c2846a013be19c769106f903c9
sfran112 <78272407+sfran112@users.noreply.github.com>
2025-01-25 02:46:12 -0500

    Updated for website deploymnet

--- shaun-react-portfolio/src/components/Welcome/Welcome.jsx
+++ shaun-react-portfolio/src/components/Welcome/Welcome.jsx
@@ -7,7 +7,7 @@ export const Welcome = () => {
     return (
     <section className={styles.container}>
         <div className={styles.content}>
-            <h1 className={styles.title}>Hello, I'm Shaun Francis</h1>
+            <h1 className={styles.title}>Hello, I'm Shaun Francis!</h1>
             <p className={styles.description}>I'm a recent Computer Science graduate who loves to code! Please reach out if you'd like to learn more!</p>
             <a href="mailto:shaun.akash@gmail.com" className={styles.contactBtn}>Contact Me</a>
         </div>
